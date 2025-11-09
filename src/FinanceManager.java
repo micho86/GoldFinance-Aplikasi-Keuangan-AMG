7aeb799fac6a12c373d964e690d6ef7242e2ac00
micho86 <64905516+micho86@users.noreply.github.com>
2025-11-09 21:16:12 +0800

    Update FinanceManager.java

--- src/FinanceManager.java
+++ src/FinanceManager.java
@@ -1,37 +1,24 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class FinanceManager {
-    private ArrayList<Transaction> transactions;
+    private final List<Transaction> transactions;
 
     public FinanceManager() {
         transactions = new ArrayList<>();
     }
 
-    public void addTransaction(Transaction t) {
-        transactions.add(t);
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
     }
 
-    public void updateTransaction(int index, Transaction t) {
-        transactions.set(index, t);
-    }
-
-    public void deleteTransaction(int index) {
-        transactions.remove(index);
-    }
-
-    public ArrayList<Transaction> getTransactions() {
+    public List<Transaction> getTransactions() {
         return transactions;
     }
 
     public double getTotalBalance() {
-        double total = 0;
-        for (Transaction t : transactions) {
-            if (t.getType().equalsIgnoreCase("Pemasukan")) {
-                total += t.getAmount();
-            } else {
-                total -= t.getAmount();
-            }
-        }
-        return total;
+        return transactions.stream()
+                .mapToDouble(Transaction::getEffectiveAmount)
+                .sum();
     }
 }
